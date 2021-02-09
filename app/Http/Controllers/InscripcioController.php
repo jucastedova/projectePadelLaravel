126e9ada7b6cf502077c82c2c8667a00dd360b82
jucastedova <100007157.joan23@fje.edu>
2021-02-09 13:56:55 +0100

    Fa la inscripció correctament

--- app/Http/Controllers/InscripcioController.php
+++ app/Http/Controllers/InscripcioController.php
@@ -21,20 +21,21 @@ class InscripcioController extends Controller
         $diferenciaDates = $dataActual - strtotime($data_naixement);
         $diferenciaAnys = $diferenciaDates / (60 * 60 * 24 * 365);
         if ($diferenciaAnys < 12) {
-            return 'CATID_SUB12'; // id_categoria 1 --> categoria sub-12
+            return '1'; // id_categoria 1 --> categoria sub-12
         } else if ($diferenciaAnys < 18) {
-            return 'CATID_SUB18'; // id_categoria 2 --> categoria sub-18
+            return '2'; // id_categoria 2 --> categoria sub-18
         } else if ($diferenciaAnys < 30) {
-            return 'CATID_SUB30'; // id_categoria 3 --> categoria sub-30
+            return '3'; // id_categoria 3 --> categoria sub-30
         } else if ($diferenciaAnys < 60) {
-            return 'CATID_SUB60'; // id_categoria 4 --> categoria sub-60
+            return '4'; // id_categoria 4 --> categoria sub-60
         } else {
-            return 'CATID_VETERA'; // id_categoria 5 --> categoria veterà
+            return '5'; // id_categoria 5 --> categoria veterà
         }
     }
 
     public function inscription(Request $request) {
         $data = $request->except('_token');
+
         try {
             // taula tbl_participant
             DB::table('tbl_participants')->insertGetId([
@@ -45,42 +46,25 @@ class InscripcioController extends Controller
                 'segon_cognom'=>$data['segon_cognom'], 
                 'data_naixement'=>$data['data_naixement'], 
                 'email'=>$data['email'], 
-                 'sexe'=>$data['sexe']]);
+                'sexe'=>$data['sexe']]);
 
-            // REVIEW
             // tabla inscripcio
+            $dni = $data['dni'];
+            $queryIdParticipant = DB::select('SELECT id_participant FROM tbl_participants WHERE dni = "'.$dni.'"');
+            // print_r($queryIdParticipant[0]->id_participant);
+            $id_participant = $queryIdParticipant[0]->id_participant;
+            
+            $id_categ = intval($this->categoria($data['data_naixement']));
             DB::table('tbl_inscripcio')->insertGetId([
-                'dni' => $data['dni'],
-                'nom'=>$data['nom'], 
-                'primer_cognom'=>$data['primer_cognom'], 
-                'primer_cognom'=>$data['primer_cognom'], 
-                'segon_cognom'=>$data['segon_cognom'], 
-                'data_naixement'=>$data['data_naixement'], 
-                'email'=>$data['email'], 
-                 'sexe'=>$data['sexe']]);
-                
-            $id_categ = $this->categoria($data['data_naixement']);
+                'id_participant'=>$id_participant, 
+                'id_categoria'=>$id_categ]);
+            
+            return redirect('inscripcio');
 
-
-
-
-
-
-
-
-            // $query1 = "INSERT INTO `tbl_inscripcio` (`id_participant`, `id_categoria`) VALUES (?, ?);";
-            // $sentencia1=$pdo->prepare($query1);
-            // $sentencia1->bindParam(1,$id_participant);
-            // $sentencia1->bindParam(2,$id_categ);
-            // // END REVIEW
-            // return redirect('inscripcio');
-
-            // taula 
         } catch (\Throwable $th) {
-            // REVIEW (modificar!!)
             echo 'Error';
-            // END REVIEW
         }
+
 
     }
 
